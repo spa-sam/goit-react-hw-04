34f91d17e30574b0fdb513421e4e75d7c467b556
Pablo <st.spasam@gmail.com>
2024-03-04 22:00:33 +0200

    fix

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,11 +1,8 @@
-import styles from "./ImageCard.module.css";
+import css from "./ImageCard.module.css";
 
 function ImageCard({ image, onImageClick }) {
   return (
-    <div
-      className={styles.card}
-      onClick={() => onImageClick(image.urls.regular)}
-    >
+    <div className={css.card} onClick={() => onImageClick(image.urls.regular)}>
       <img src={image.urls.small} alt={image.alt_description} />
     </div>
   );
